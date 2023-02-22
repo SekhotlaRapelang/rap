bfbc7ac02e79e08298ea6f0a9a9f256c4eca9fef
Your Name <you@example.com>
2023-02-22 18:33:57 +0200

    work

---
+++ src/main/java/com/example/exercise1/HelloController.java
@@ -0,0 +1,30 @@
+
+package com.example.exercise1;
+
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Label lable;
+
+    int number = 0;
+
+    @FXML
+    void DecreaseOnAction(ActionEvent event) {
+        number = number - 1;
+        String s1 = Integer.toString(number);
+        lable.setText(s1);
+    }
+
+    @FXML
+    void IncreaseOnAction(ActionEvent event) {
+        number = number + 1;
+        String s1 = Integer.toString(number);
+        lable.setText(s1);
+    }
+
+}
+
